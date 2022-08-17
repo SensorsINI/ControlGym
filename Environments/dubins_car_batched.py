c50df4a5acd081cfd0d7286b65e772f88fa3ff9f
Frederik Heetmeyer <heerfn@yahoo.de>
2022-08-17 10:47:17 +0200

    - Update source/license detail for Dubin's Car - Update refs

--- Environments/dubins_car_batched.py
+++ Environments/dubins_car_batched.py
@@ -1,4 +1,31 @@
-# Original Source: https://github.com/gargivaidya/dubin_model_gymenv/blob/main/dubin_gymenv.py
+"""
+This file has been modified from source https://github.com/gargivaidya/dubin_model_gymenv/blob/main/dubin_gymenv.py
+
+This was in turn adapted from https://github.com/AtsushiSakai/PythonRobotics under...
+
+The MIT License (MIT)
+
+Copyright (c) 2016 - 2022 Atsushi Sakai and other contributors:
+https://github.com/AtsushiSakai/PythonRobotics/contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 from typing import Optional, Union, Tuple
 import numpy as np
