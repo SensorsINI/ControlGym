82d747df39573076699e6506e3a6863c0d048e14
EnnioEvo <artemisvii@gmail.com>
2022-11-08 19:41:55 +0100

    fixed bug to solve ambiguity between configs

--- SI_Toolkit_ASF/__init__.py
+++ SI_Toolkit_ASF/__init__.py
@@ -1,7 +1,12 @@
+import os
+
 from yaml import load, FullLoader
 
-config = load(open("config.yml", "r"), Loader=FullLoader)
-
+# config = load(open("../config.yml", "r"), Loader=FullLoader)
+try:
+    config = load(open(os.path.join(os.path.abspath("."), "config.yml"), "r"), Loader=FullLoader)
+except:
+    config = load(open("../../config.yml", "r"), Loader=FullLoader)
 
 GLOBALLY_DISABLE_COMPILATION = config["1_data_generation"]["debug"]
 USE_JIT_COMPILATION = config["1_data_generation"]["use_jit_compilation"]
